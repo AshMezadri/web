139ca013b0d062dd530c32a72385d28a56e258d7
Ash <ash.m10@aluno.ifsc.edu.br>
2023-10-02 10:50:02 -0300

    aulaa

---
+++ aulaJS/main.js
@@ -0,0 +1,12 @@
+console.log(window.location);
+
+let p = document.getElementById("p1");
+
+function f1(){
+    p.innerHTML = "bla";
+}
+
+function nome(){
+    let nome = prompt("Qual é seu nome?");
+    console.log(nome);
+}
