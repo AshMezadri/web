e8abddd1dde77eb75bf01b93f7c2ba9bed3204c2
Ash <ash.m10@aluno.ifsc.edu.br>
2023-10-02 11:04:46 -0300

    contador

--- aulaJS/main.js
+++ aulaJS/main.js
@@ -1,12 +1,8 @@
 console.log(window.location);
 
-let p = document.getElementById("p1");
+let contador = document.getElementById("cont");
+let valor = 0;
 
-function f1(){
-    p.innerHTML = "bla";
-}
-
-function nome(){
-    let nome = prompt("Qual é seu nome?");
-    console.log(nome);
+function contar(){
+    contador.innerHTML = valor++;
 }
