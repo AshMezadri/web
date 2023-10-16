ae4bc21b6598e836728d97770d0eeac6dc409c60
ash <ash.m10@aluno.ifsc.edu.br>
2023-10-16 09:53:03 -0300

    soma

--- aulaJS.02/main.js
+++ aulaJS.02/main.js
@@ -1,6 +1,14 @@
-console.log("Hello World");
+let n1 = document.getElementById('input1');
+let n2 = document.getElementById('input2');
+let resultado = document.getElementById('resultado');
 
-let a = Number(prompt("Informe um valor para n1: "));
-let b = Number(prompt("Informe um valor para n2: "));
 
-alert(a+b);
+function calcular(){
+    
+    let input1 = Number(n1.value);
+    let input2 = Number(n2.value);
+
+    soma = input1 + input2;
+
+    resultado.innerHTML = soma;
+}
