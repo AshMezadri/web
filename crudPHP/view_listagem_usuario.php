39dc6ecc97a2a37bd1c435d2ff3d5de53dc8fb86
ash <ash.m10@aluno.ifsc.edu.br>
2023-11-13 11:02:31 -0300

    ++

---
+++ crudPHP/view_listagem_usuario.php
@@ -0,0 +1,16 @@
+<table id="lista_usuarios">
+    <thead>
+        <tr>
+
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Senha</th>
+            <th>Editar</th>
+            <th>Excluir</th>
+
+        </tr>
+    </thread>
+    <tbody>
+        
+    </tbody>
+</table>
